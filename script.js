ee7aff201dec123f416b85a57db5d96aabcf7810
Siarhei Labuts <sergeylabut@gmail.com>
2020-09-03 21:26:58 +0300

    init

---
+++ script.js
@@ -0,0 +1,47 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  const baseDelay = 0.5;
+  const divider = 7;
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    burger.classList.toggle("toggle");
+
+    navLinks.forEach((link, i) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          i / divider + baseDelay
+        }s`;
+      }
+    });
+  });
+};
+
+navSlide();
